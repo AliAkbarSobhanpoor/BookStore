2cb27a5ecf995e3a575b1d3f900d1c8a06d9ab80
aliakbarsobhanpour <aliakbarsobhanpoor@gmail.com>
2025-02-28 18:56:08 +0330

    display book model in the admin . and also fix some js issue for cleaning new created input value

--- literature/admin.py
+++ literature/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    model = models.Book
+    list_display = [
+        "name",
+        "isbn_number"
+    ]
+admin.site.register(models.Book, BookAdmin)
